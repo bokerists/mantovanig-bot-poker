94feca20f575303dcc5e353a8727a043ed40ff01
Mantovani Gabriele <gabriele.mantovani92@gmail.com>
2016-10-05 19:40:52 +0200

    fix

--- engine.js
+++ engine.js
@@ -1,13 +1,30 @@
+const _ = require('lodash');
+
 exports = module.exports = {
 
     init: function(gamestate) {
 
           var me = gamestate.me;
           var myCard = gamestate.players[me].cards;
+          var callAmount = gamestate.callAmount;
 
-          console.log('my card', myCard);
+          //console.log('my card', myCard);
+
+          if(this.checkCoppia(myCard))
+              return callAmount;
+
 
           return 0;
+
+    },
+
+      checkCoppia: function(myCard) {
+
+          // var myCardRank = myCard.map((cards) => return cards.rank);
+
+         var grouped =_.values(_.groupBy(myCard, 'rank'));
+
+        return  grouped.filter((g) => g.length == 2).length > 0;
 
     }
 
